cc4525ce2971c292248c517465b57a9c9600f935
larissa <souzalarissa383@gmail.com>
2020-03-08 00:21:50 -0300

    Initial commit

---
+++ banco-rupee.sql
@@ -0,0 +1,41 @@
+﻿-- Database: "banco-rupee"
+
+-- DROP DATABASE "banco-rupee";
+
+CREATE DATABASE "banco-rupee"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Portuguese_Brazil.1252'
+       LC_CTYPE = 'Portuguese_Brazil.1252'
+       CONNECTION LIMIT = -1;
+
+CREATE TABLE cadastro (
+	  id int ,
+	  nome varchar (15),
+	  sobrenome varchar(15) NOT NULL,
+	  usuario varchar(15) NOT NULL,
+	  senha VARCHAR(20) NOT NULL,
+	  email varchar(20) NOT NULL,
+	  endereco varchar(20) NOT NULL,
+	  cidade varchar(20) NOT NULL,
+	  estado varchar(20) NOT NULL,
+	  primary key (id)
+  );
+
+  CREATE TABLE cadprojeto (
+	  id int,
+	  nome varchar(20) NOT NULL,
+	  endereco varchar(15) NOT NULL,
+	  cidade varchar(15) NOT NULL,
+	  estado varchar(15) NOT NULL,
+	  primary key (id)
+	  );
+
+ CREATE TABLE usuario (
+  id_usuario int,
+  nome varchar(30) NOT NULL,
+  login varchar(30) NOT NULL,
+  senha varchar(30) NOT NULL,
+  primary key (id)
+	  );
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjetoRupee::Application.config.secret_token = 'ba517b342af714c8e6fc19b416067f110a29f743e63dcfe6c2b30ffe18be8f112dae90c5bd963bbf61ade0a48c906aae939439753f26a71b95bba8e566c6c208'
